d291e33dd39f592f33d638772065aefe67c4158a
Javier Manzano <javier@brainattica.com>
2015-04-03 21:36:12 +0200

    init first activity

---
+++ app/src/main/java/brainattica/com/rsasample/ui/activities/MainActivity.java
@@ -0,0 +1,20 @@
+package brainattica.com.rsasample.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import brainattica.com.rsasample.R;
+
+/**
+ * Created by javiermanzanomorilla on 03/04/15.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}

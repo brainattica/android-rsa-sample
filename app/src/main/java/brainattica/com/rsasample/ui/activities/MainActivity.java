713adeaf3cbfbf0ba28fc41ee8a06d12465ca599
Javier Manzano <javier@brainattica.com>
2015-04-04 12:27:53 +0200

    layout

--- app/src/main/java/brainattica/com/rsasample/ui/activities/MainActivity.java
+++ app/src/main/java/brainattica/com/rsasample/ui/activities/MainActivity.java
@@ -2,6 +2,7 @@ package brainattica.com.rsasample.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
 
 import brainattica.com.rsasample.R;
 
@@ -10,10 +11,19 @@ import brainattica.com.rsasample.R;
  */
 public class MainActivity extends ActionBarActivity {
 
+    private Button generate;
+
+    private Button encryptOrDecrypt;
+
+    private Button signOrVerify;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        generate = (Button) findViewById(R.id.generate_rsa);
+        encryptOrDecrypt = (Button) findViewById(R.id.encrypt_decrypt);
+        signOrVerify = (Button) findViewById(R.id.sign_verify);
     }
 
 

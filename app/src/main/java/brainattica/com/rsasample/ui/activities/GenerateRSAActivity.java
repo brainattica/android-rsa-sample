0a3f33fbe5cc40ac8d462812750d6754ea66a5fa
Javier Manzano <javier@brainattica.com>
2015-05-07 18:15:44 +0200

    generating RSA on new activity

--- app/src/main/java/brainattica/com/rsasample/ui/activities/GenerateRSAActivity.java
+++ app/src/main/java/brainattica/com/rsasample/ui/activities/GenerateRSAActivity.java
@@ -4,16 +4,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import brainattica.com.rsasample.R;
+import brainattica.com.rsasample.crypto.RSA;
 
 
 public class GenerateRSAActivity extends ActionBarActivity {
+
+    private ProgressBar progress;
+
+    private TextView generated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_rs);
+        progress = (ProgressBar) findViewById(R.id.progress);
+        generated = (TextView) findViewById(R.id.generated);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                RSA.generate();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progress.setVisibility(View.GONE);
+                        generated.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        }).start();
     }
 
 }
